c578b959f387466d4cb0d85111b95841cd64e058
Kaue <kauecouto19.kc@gmail.com>
2023-01-02 22:35:07 -0300

    modificações no ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB3dRlIcG_MvhBFi-MIDmBoZVk0EuFZnBU",
+    authDomain: "projeto-study-2ed9d.firebaseapp.com",
+    projectId: "projeto-study-2ed9d",
+    storageBucket: "projeto-study-2ed9d.appspot.com",
+    messagingSenderId: "321672880410",
+    appId: "1:321672880410:web:7bdd3a14d6c713426a4ca3"
+  },
 };
