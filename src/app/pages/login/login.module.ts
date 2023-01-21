9253226b56eb35d7e5c3b4e538affd902756a8ee
Kaue <kauecouto19.kc@gmail.com>
2023-01-21 14:46:28 -0300

    refatorando

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,6 +12,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     FormsModule
   ]
 })
